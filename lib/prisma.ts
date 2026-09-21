78f1230f8f7d447b8c9df55f3bf4f051a7461bfa
Andre Cosmos <andrecosmos.1992@gmail.com>
2026-09-21 12:15:32 -0300

    v0

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof prismaClientSingleton> | undefined
 }
 
+
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 export default prisma
